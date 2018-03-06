f55e19be5d319145a2246da5c22ae50ea1db24cf
loktionov129 <loktionov129@gmail.com>
2018-03-06 21:03:17 +0700

    Added buttons for minimize/maximize subtasks in NavMenu

--- TaskManager/ClientApp/components/TasksTree.tsx
+++ TaskManager/ClientApp/components/TasksTree.tsx
@@ -7,22 +7,42 @@ import { Task, TaskWithChildren } from '../shared/models/Task';
 
 interface TasksTreeProps {
     root: TaskWithChildren;
+    isFolded: boolean;
 }
 
 class TasksTree extends React.Component<TasksTreeProps, {}> {
+    constructor() {
+        super();
+        this.toggle = this.toggle.bind(this);
+    }
+    
+    public toggle(event: any) {
+        (event.target as any).parentNode.nextSibling.classList.toggle("folded");
+        event.target.className = event.target.className === 'text-success glyphicon glyphicon-chevron-down'
+            ? 'text-success glyphicon glyphicon-chevron-right'
+            : 'text-success glyphicon glyphicon-chevron-down'
+    }
+    
     public render(): JSX.Element {
-        const { root } = this.props;
+        const { root, isFolded } = this.props;
 
         if (root.children.length === 0) {
             return <div />;
         }
 
-        return <ul className='tree'>
+        return <ul className={'tree' + (isFolded ? '' : ' folded')}>
             { root.children.map(tree => [
                 <li key={tree.id}>
-                    <NavLink to={'/task/' + tree.id}>{ tree.title }</NavLink>
+                    {tree.children.length > 0 &&
+                        <span
+                            title='Toggle tree'
+                            className='text-success glyphicon glyphicon-chevron-right'
+                            onClick={this.toggle}
+                        ></span>
+                    }
+                    &nbsp; <NavLink to={'/task/' + tree.id}>{ tree.title }</NavLink>
                 </li>,
-                <TasksTree root={tree} />
+                <TasksTree root={tree} isFolded={false} />
             ])}
         </ul>;
     }
@@ -31,7 +51,7 @@ class TasksTree extends React.Component<TasksTreeProps, {}> {
 @observer
 export class TasksTreeContainer extends React.Component<{}, {}> {
     public render() {
-        return <TasksTree root={TaskStore.tree} />;
+        return <TasksTree root={TaskStore.tree} isFolded={true} />;
     }
 
     // fix for router
